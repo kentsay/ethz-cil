0a0fd7657d50bbfbbd71437409d69523a5373da9
Rolf Jagerman <rjagerman@gmail.com>
2015-06-05 15:12:26 +0200

    Improved priority smoothing algorithm

--- inpainting/smoothing/prioritysmoothing.m
+++ inpainting/smoothing/prioritysmoothing.m
@@ -1,10 +1,14 @@
 function [ I_rec ] = prioritysmoothing( I, mask, window_size, alpha )
-%priority_smoothing A smoothing algorithm based on a priority inpainting
-%   Inpainting happens on a smooth basis (mean of pixels around it),
-%   starting with pixels that we are sure about (i.e. those that have a lot
-%   of known pixels around it (edges) and whose surrounding pixels are
-%   mostly the same color (max(pixels) - min(pixels) is small)
+%% PRIORITYSMOOTHING
+% A smoothing algorithm based on a priority inpainting
+% Inpainting happens on a smooth basis (mean of pixels around it),
+% starting with pixels that we are sure about (i.e. those that have a lot
+% of known pixels around it (edges) and whose surrounding pixels are
+% mostly the same color (max(pixels) - min(pixels) is small)
 
+%% Order pixels we want to inpaint by priority
+%  The priority is determined by the amount of known pixels around it and
+%  the difference of the highest and smallest pixel value around it.
 beta = 1 - alpha;
 priority = zeros(size(I,1)^2, 3);
 count = 0;
@@ -28,29 +32,31 @@ for i = 1:size(I,1)
     end
 end
 
+%% Sort pixels to fill in by highest priority first
+%  This makes it possible to start with pixels that have a a lot of known
+%  pixels around them.
 [~, order] = sort(priority(:,3), 'descend');
 priority = priority(order,:);
 
+%% Fill in pixels and construct I_rec
+%  Fills in the pixel using the mean of the pixels around it
+inpainted = mask~=0;
 I_rec = I;
 for it = 1:count
     test = priority(it,:);
     i = test(1);
     j = test(2);
-    p = test(3);
-    image_window = get_window_pixels(I, i, j, window_size);
-    new_value = mean(image_window(get_window_pixels(mask, i, j, window_size)~=0));
-    if isnan(new_value)
-        new_value = mean(mean(image_window));
-    end
-    if size(new_value, 1)==0
+    image_window = get_window_pixels(I_rec, i, j, window_size);
+    new_value = mean(image_window(get_window_pixels(inpainted, i, j, window_size)~=0));
+    if isnan(new_value) || size(new_value, 1)==0
         new_value = mean(mean(image_window));
     end
     I_rec(i,j) = new_value;
+    mask(i,j) = 1;
 end
 
+%% Helper function to get a window of pixels
 function [I_window] = get_window_pixels(I, x, y, window)
-% Helper function to easily get a window within the pixel region of an
-% image
     I_window = I(max(1, x - window):min(size(I,1), x + window), max(1, y - window):min(size(I,1), y + window));
 end
 
