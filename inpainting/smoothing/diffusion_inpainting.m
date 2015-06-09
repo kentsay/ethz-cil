9ac551352f71b6ce6ec659f875a35f0469cf48f3
Rolf Jagerman <rjagerman@gmail.com>
2015-06-09 14:08:57 +0200

    Added diffusion inpainting (fastest, best performing so far)

---
+++ inpainting/smoothing/diffusion_inpainting.m
@@ -0,0 +1,20 @@
+function [ I_rec ] = diffusion_inpainting( I, mask, T )
+    %% DIFFUSION
+    % A diffusion based inpainting algorithm
+    I_rec = I;
+    
+    %% Set parameters and construct kernel
+    a = 0.073235;
+    b = 0.176765;
+    K = [a b a; b 0 b; a b a];
+
+    %% Fill in pixels and construct I_rec
+    %  Fills in the pixel using the mean of the pixels around it
+    %K = fspecial('gaussian', window_size * 2 + 1);
+    for t = 1:T
+        I_rec = imfilter(I_rec, K, 'replicate');
+        I_rec = (double(mask==0) .* I_rec) + (double(mask~=0) .* I);
+    end
+
+end
+
