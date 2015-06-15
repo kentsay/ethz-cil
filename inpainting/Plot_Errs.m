493efd2c9ce632705b62b707c81cc8992f31ca3f
jderiu <jderiu@student.ethz.ch>
2015-06-15 14:29:31 +0200

    stuff really just stuff + code for plotting errs

---
+++ inpainting/Plot_Errs.m
@@ -0,0 +1,65 @@
+%% Script to create plots including different algorithms. The user defines all the subfolders needed for the plot. This script is based on the EvaluateInpainting.m provided in the course.
+
+
+dirs = {'dictionary_learning'};
+
+
+for curr_dir_number = 1:length(dirs)
+    curr_dir = dirs{curr_dir_number};
+    file_list = dir(curr_dir); 
+    k = 1;
+
+    Errors = []; % mean squared errors for each image would be stored here
+    Times = [];
+    n_it = 9;
+    miss_interval = linspace(0.1,0.9,n_it);
+    Result = zeros(1,n_it);
+    Time = zeros(1,n_it);
+    for j=1:n_it
+        for i = 3:length(dir) % running through the folder
+            tic;
+            file_name = file_list(i).name; % get current filename
+
+            % Only keep the images in the loop
+            if (length(file_name) < 5)
+                continue;
+            elseif ( max(file_name(end-4:end) ~= '2.png'))
+                continue;
+            end
+            mask_name = [file_name(1:end-4) '_mask.png'];
+
+            % Read image, convert to double precision and map to [0,1] interval
+            I = imread(file_name); 
+            I = double(I) / 255; 
+
+            % Read the respective binary mask
+            % EVALUATION IS DONE WITH A FIXED MASK
+            %mask = imread(mask_name);
+
+
+            mask = random_mask(512,miss_interval(j));
+            I_mask = I;
+            I_mask(~mask) = 0;
+            %figure;
+            %imshow(I_mask); 
+            % Call the main inPainting function
+            cd(curr_dir);
+            I_rec = inPainting(I_mask, mask);
+            cd('../')
+            %figure;
+            %imshow(I_rec);
+            % Measure approximation error
+            Errors(k) = mean(mean(mean( ((I - I_rec) ).^2)));
+            Times(k) = toc;
+            k = k+1;
+
+        end
+        err = mean(Errors);
+        Result(j) = err;
+        Time(j) = mean(Times);
+    end
+    name = strcat(curr_dir,'.mat');
+    save(name,'Result','Time','miss_interval');
+end
+
+
